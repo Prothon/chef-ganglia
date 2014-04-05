b0d86788f4ea7bbda07524b65a3004fade7316cb
raymer <AndrewRaymer@gmail.com>
2014-04-05 18:20:46 -0400

    first commit

---
+++ recipes/server.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook name: ganglia
+# Recipe:: server
+
+node['ganglia']['server']['packages'].each do |gangliapkgs|
+    package gangliapkgs
+end
+
+service "httpd" do
+    action [:start, :enable]
+end
+
+service "gmond" do
+    action [:start, :enable]
+end
+
+service "gmetad" do
+    action [:start, :enable]
+end
+
+template "/etc/ganglia/gmetad.conf" do
+    source "gmetad.conf.erb"
+    mode "0644"
+    owner "root"
+    group "root"
+    notifies :restart, "service[gmetad]", :immediately
+end
+
+template "/etc/ganglia/gmond.conf " do
+    source "gmond.conf.server.erb"
+    mode "0644"
+    owner "root"
+    group "root"
+    notifies :restart, "service[gmond]", :immediately
+end
+
+template "/etc/httpd/conf.d/ganglia.conf " do
+    source "ganglia.conf.erb"
+    mode "0644"
+    owner "root"
+    group "root"
+    notifies :restart, "service[httpd]", :immediately
+end
